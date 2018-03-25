5d59b4242f3e458f1b131637bd0b6daf15f46226
Paul Norman <penorman@mac.com>
2018-03-25 02:24:34 -0700

    Filter out not-languages when extracting names

--- sql/names.sql
+++ sql/names.sql
@@ -11,8 +11,10 @@ CREATE OR REPLACE FUNCTION extract_names (tags hstore)
           FROM each(tags)
           WHERE key LIKE 'name:%'
       ) AS s(key, value)
+      WHERE key NOT LIKE '%:%' -- Anything with a : still is a multi-level one, and not a simple language code
+      AND key !~ '.*\d.*' -- matches something like name:en1
+      AND key NOT IN ('prefix', 'genitive', 'etymology', 'botanical', 'source', 'left', 'right') -- blacklist common not-languages
   $$
 LANGUAGE SQL
 IMMUTABLE
 STRICT;
-
